e8080307f9201a08d4358f66bbcaf7ef33e5d332
Wood Wu <wood.wu@elephanttalk.com>
2016-05-29 00:14:58 +0800

    Initialize solution

---
+++ Rld.Acs.WebService.Contract/ISample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rld.Acs.WebService.Contract
+{
+    public interface ISample
+    {
+        void DoSomething();
+    }
+}
---
+++ Rld.Acs.WebService/SampleService.svc.cs
@@ -0,0 +1,19 @@
+﻿using Rld.Acs.WebService.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Rld.Acs.WebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SampleService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SampleService.svc or SampleService.svc.cs at the Solution Explorer and start debugging.
+    public class SampleService : ISample
+    {
+        public void DoSomething()
+        {
+        }
+    }
+}
