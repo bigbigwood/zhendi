d676af9d481919cb44a99420d2340e6775695a60
Wood Wu <wood.wu@elephanttalk.com>
2016-10-11 14:26:06 +0800

    修正PDF文件不能显示中文问题

--- Rld.Acs.WpfApplication/Service/ExportFile/PdfService.cs
+++ Rld.Acs.WpfApplication/Service/ExportFile/PdfService.cs
@@ -1,31 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
+using log4net;
+using Font = iTextSharp.text.Font;
 
 namespace Rld.Acs.WpfApplication.Service.ExportFile
 {
     public class PdfService
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void ExportDataTable(DataTable datatable, string fileName)
         {
             try
             {
-
                 Document document = new Document();
                 using (FileStream file = new FileStream(fileName, FileMode.Create, System.IO.FileAccess.Write))
                 {
                     PdfWriter.GetInstance(document, file);
                     document.Open();
 
-                    Font myFont = FontFactory.GetFont("Arial", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-                    //BaseFont bfChinese = BaseFont.CreateFont("C:\\WINDOWS\\Fonts\\STSONG.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-                    //Font fontChinese = new Font(bfChinese, 12);
+                    //var fontName = System.Drawing.SystemFonts.DefaultFont.Name;
+                    //Log.InfoFormat("系统默认字体为:{0}", fontName);
+                    //Font myFont = FontFactory.GetFont("微软雅黑", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+                    BaseFont bfChinese = BaseFont.CreateFont(Environment.CurrentDirectory + @"/Resources/ARIALUNI.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                    Font myFont = new Font(bfChinese, 12);
 
                     PdfPTable table = new PdfPTable(datatable.Columns.Count);
                     //table header
